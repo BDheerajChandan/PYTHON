640ce55676b7211d302978d347edb8086ebbddf1
B.DHEERAJ CHANDAN <88262353+BDheerajChandan@users.noreply.github.com>
2021-09-19 10:39:39 +0530

    Add files via upload
    Write a code in Python to check a number is an even or odd or not.

---
+++ EvenOrOdd_py_3.py
@@ -0,0 +1,7 @@
+# Write a code in Python to check a number is an even or odd or not.
+print('Enter the number : ',end='')
+n=int(input())
+if n%2==0:
+	print(n,' : is an even number')
+else:
+	print(n,' : is an odd number')
